59add8d140fbc16f85a507aaf3864f1cf31856ea
EliUmrani <167150327+EliUmrani@users.noreply.github.com>
2024-04-18 07:13:18 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 function flipCard(cardBox) {
     cardBox.querySelector('.card').classList.toggle('flipped');
   }  
+
+document.addEventListener('DOMContentLoaded', function() {
+  const urls = [
+    "https://www.bowlero.com/location/bowlero-cary",
+    "https://theboxcarbar.com/raleigh/"
+  ];
+
+  const randomButton = document.getElementById('randomButton');
+
+  randomButton.addEventListener('click', function() {
+    const randomIndex = Math.floor(Math.random() * urls.length);
+    const randomUrl = urls[randomIndex];
+    window.location.href = randomUrl;
+  });
+});
